ed738b627e6c3027e5849b768c89735285a74d2f
jakemager <jmager12@gmail.com>
2018-03-05 17:10:59 -0500

    Comined everything and added navigation

---
+++ js/volunteer.js
@@ -0,0 +1,80 @@
+/*
+  volunteer.js
+  by Jake Mager 3/2/2018
+  
+  * Dynamically add/remove volunteer 'forms' to the volunteerHolder div
+  * Volunteer page has a hidden input form that holds the amount of volunteers - 1 
+  * Each div has an id_n id being input name, n being amount of volunteers - 1
+  * Easily go through this with a for loop
+*/
+
+$( document ).ready(function() {
+
+    // addVolunteer event listener 
+    // appends new volunteerInput_ID formDiv to volunteerHolder div
+    $('#addVolunteer').on('click touch', function() {
+
+        // get count of volunteer from hidden div
+        var volunteerCount = parseInt($("#volunteerCount").val()) + 1;
+        
+        // max 10 volunteers
+        if (volunteerCount > 9) {
+            alert("Max volunteers met!");
+            return;
+        } 
+
+        // create html for volunteer form
+        var volunteerHTML = "<div id='volunteerInput_" + volunteerCount + "'>" +
+            "<div class='row volunteerRow'>" +
+                "<div class='itemLabel col-xs-6'>Name</div>" +
+                "<div class='col-xs-6'>" +
+                    "<input type='text' id='volunteerName_" + volunteerCount + "'/>" +
+                "</div>" +
+            "</div>" +
+            "<div class='row volunteerRow'>" +
+                "<div class='itemLabel col-xs-6'>Date</div>" +
+                "<div class='col-xs-6'>" +
+                    "<input type='date' id='volunteerDate_" + volunteerCount + "' value='" + $("#volunteerDate_0").val() + "'/>" +
+                "</div>" +
+            "</div>" +
+            "<div class='row volunteerRow'>" +
+                "<div class='itemLabel col-xs-6'>Activity</div>" +
+                "<div class='col-xs-6'>" +
+                    "<input type='text' id='volunteerActivity_" + volunteerCount + "'/>" +
+                "</div>" +
+            "</div>" +
+            "<div class='row volunteerRow volunteerRowBottom'>" +
+                "<div class='itemLabel col-xs-6'>Hours</div>" +
+                "<div class='col-xs-6'>" +
+                    "<input type='number' id='volunteerHour_" + volunteerCount + "'/>" +
+                "</div>" +
+            "</div>" +
+        "</div>";
+        
+        // save new count to hidden div
+        $("#volunteerCount").val(volunteerCount);
+
+        // put html into volenteerHolder div
+        $("#volunteerHolder").append(volunteerHTML);
+
+        // scroll down page to show removeVolenteer btn
+        $('html, body').animate({
+        scrollTop: $("#removeVolunteer").offset().top
+        }, 1000);
+
+    });
+
+    // removeVolunteer button event listener
+    // fades then deletes the last volunteerHolder div 
+    // and decreases voulunteerCount hidden div
+    $('#removeVolunteer').on('click touch', function() {
+        // get count of volunteer
+        var volunteerCount = parseInt($("#volunteerCount").val());
+
+        if (volunteerCount != 0) {
+        $("#volunteerInput_" + volunteerCount).fadeOut();
+        $("#volunteerInput_" + volunteerCount).remove();
+        $("#volunteerCount").val(volunteerCount - 1);
+        }
+    });
+})
